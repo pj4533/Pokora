e73135c00d4d646b096816b04fff8e3d03e4022b
PJ Gray <pj@evertrue.com>
2023-02-22 12:24:04 -0500

    Initial Commit

---
+++ Pokora/PokoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokoraApp.swift
+//  Pokora
+//
+//  Created by PJ Gray on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PokoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
