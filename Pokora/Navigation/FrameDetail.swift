7be83c33b94a4894eec454df23bbf4218c0d52b0
PJ Gray <pj@evertrue.com>
2023-04-21 10:52:44 -0400

    Changed to one frame, added toolbar. Closes #21

--- Pokora/Navigation/FrameDetail.swift
+++ Pokora/Navigation/FrameDetail.swift
@@ -13,34 +13,12 @@ struct FrameDetail: View {
     @State private var strengthString = "0.2"
     let seed: UInt32 = UInt32.random(in: 0...UInt32.max)
     @ObservedObject var store: VideoStore
+    @State private var showProcessed = false
 
     var body: some View {
+        let url = showProcessed ? store.video?.frames[frameIndex-1].processed?.url ?? store.video?.frames[frameIndex-1].url : store.video?.frames[frameIndex-1].url
         VStack {
-            HStack {
-                FrameImageView(imageUrl: store.video?.frames[frameIndex-1].url, emptyStateString: "No frame loaded")
-                Button("Process") {
-                    if store.pipeline == nil {
-                        do {
-                            try store.initializePipeline()
-                        } catch let error {
-                            print("ERROR INIT PIPELINE: \(error)")
-                        }
-                    }
-                    if let url = store.video?.frames[frameIndex-1].url, let strength = Float(strengthString) {
-                        var processedFrame = ProcessedFrame(seed: seed, prompt: prompt, strength: strength)
-                        do {
-                            processedFrame.url = try store.process(imageUrl: url, prompt: prompt, strength: strength, seed: seed)
-                            store.video?.frames[frameIndex-1].processed = processedFrame
-                        } catch let error {
-                            print(error)
-                        }                        
-                    } else {
-                        print("Error during processing")
-                    }
-                }
-                .frame(width: 80.0)
-                FrameImageView(imageUrl: store.video?.frames[frameIndex-1].processed?.url, emptyStateString: "Frame not processed")
-            }
+            FrameImageView(imageUrl: url, emptyStateString: "No frame loaded")
             HStack {
                 Text("Prompt: ")
                     .fixedSize()
@@ -55,6 +33,40 @@ struct FrameDetail: View {
             }
         }
         .padding()
+        .toolbar {
+            ToolbarItemGroup(placement: .principal) {
+                Toggle(isOn: $showProcessed) {
+                    Label("Show Processed Image", systemImage: "sparkles")
+                }
+                .toggleStyle(.button)
+                .disabled(store.video?.frames[frameIndex-1].processed?.url == nil)
+                Menu("Process") {
+                    Button("Process All") {
+                    }
+                } primaryAction: {
+                    if store.pipeline == nil {
+                        do {
+                            try store.initializePipeline()
+                        } catch let error {
+                            print("ERROR INIT PIPELINE: \(error)")
+                        }
+                    }
+                    if let url = store.video?.frames[frameIndex-1].url, let strength = Float(strengthString) {
+                        var processedFrame = ProcessedFrame(seed: seed, prompt: prompt, strength: strength)
+                        do {
+                            processedFrame.url = try store.process(imageUrl: url, prompt: prompt, strength: strength, seed: seed)
+                            store.video?.frames[frameIndex-1].processed = processedFrame
+                            showProcessed = true
+                        } catch let error {
+                            print(error)
+                        }
+                    } else {
+                        print("Error during processing")
+                    }
+                }
+                .menuStyle(.button)
+            }
+        }
     }
 }
 
